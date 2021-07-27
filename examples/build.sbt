38ed732d88243fc630ef93be3d5e71e342894afb
Abhay Kulkarni <developer.ak23@gmail.com>
2021-07-27 08:22:52 +0530

    Moving the projects under a new folder

--- examples/build.sbt
+++
@@ -1,4 +0,0 @@
-name := "examples"
-version := "0.1"
-scalaVersion := "2.13.6"
-libraryDependencies += "org.scalatest" %% "scalatest" % "3.3.0-SNAP3" % Test
--- examples/src/main/scala/Person.scala
+++
@@ -1,7 +0,0 @@
-case class Person (firstName: String, lastName: String, age: Int) {
-  def description: String = s"Hi, I am ${firstName} ${lastName}. I am $age years old."
-}
-
-object Person {
-  def filterAdult(people: List[Person]) : List[Person] = people.filter(p => p.age > 20)
-}
