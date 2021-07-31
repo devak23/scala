f2e61ad7e7c77fb51d35c819a92ced67c5dec9bd
Abhay Kulkarni <developer.ak23@gmail.com>
2021-07-31 18:46:07 +0530

    added another handy method to make it more clean

--- scala-projects/fundamentals/src/main/scala/challenge_01/Implementation.scala
+++ scala-projects/fundamentals/src/main/scala/challenge_01/Implementation.scala
@@ -6,7 +6,8 @@ class Implementation extends Abstract {
   def sum(x: Int, y: Int): Int = x + y
   def difference(x: Int, y: Int): Int = if (x == y) 0 else if (x > y) x - y else y - x
   def product(x: Int, y: Int): Int = x * y
-  def roundToEven(a: Int, b: Int): (Int, Int) = (if (a % 2 == 0) a else a + 1, if (b % 2 == 0) b else b - 1)
+  def isEven(x: Int): Boolean = x %2 == 0
+  def roundToEven(a: Int, b: Int): (Int, Int) = (if (isEven(a)) a else a + 1, if (isEven(b)) b else b - 1)
 
   @tailrec
   final def sumEven (a: Int, b: Int, partialSum: Int): Int = {
